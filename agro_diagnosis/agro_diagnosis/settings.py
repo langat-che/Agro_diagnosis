7b475275ce8a3a9a3b13240e7d852bf529d1688e
langat-che <langatchebetbev@gmail.com>
2025-05-05 01:13:34 +0300

    Update 3 on deployment files

--- agro_diagnosis/agro_diagnosis/settings.py
+++ agro_diagnosis/agro_diagnosis/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://agrodiagnosis-production.up.railway.app/').split(',')
 
 
