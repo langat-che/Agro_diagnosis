10db95e0dfcb67561251f565159301aa22aaeb15
langat-che <langatchebetbev@gmail.com>
2025-05-06 12:17:46 +0300

    Added .slugignore file

--- agro_diagnosis/settings.py
+++ agro_diagnosis/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = False
+DJANGO_ENV='development'
+DEBUG = True if os.environ.get('DJANGO_ENV') == 'development' else False
 
 
 # allowed hosts
